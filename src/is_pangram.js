d1de39e233beaeb3ecf24bb58c754b41b41b58de
Li Dai <li.lea.dai@gmail.com>
2020-12-10 15:48:01 -0800

    tests for pangram

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,22 @@
-const isPangram = function(text) {
+const alphabets = 'abcdefghijklkmnopqrstuvwxyz'.split('');
+const checker = {};
 
+const isPangram = function(text) {
+  alphabets.forEach(alphabet => {
+    checker[alphabet] = 0;
+  });
+
+  text.split('').forEach(char => {
+    checker[char] = 1;
+  });
+
+  for (const alphabet in checker) {
+    if (checker[alphabet] === 0){
+      return false;
+    }
+  }
+  return true;
 };
+
 
 module.exports = isPangram;
